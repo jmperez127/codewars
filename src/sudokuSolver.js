a2eaabf8c5a6b9b00eb63fc2c7e5e3f5e9bc80a7
Jean <jmperez127@gmail.com>
2017-04-27 18:41:46 -0400

    Add more sudoku solver progress

--- src/sudokuSolver.js
+++ src/sudokuSolver.js
@@ -1,3 +1,30 @@
+
+var puzzle2 = [
+  [5, 3, 0, 0, 7, 0, 0, 0, 0],
+  [6, 0, 0, 1, 9, 5, 0, 0, 0],
+  [0, 9, 8, 0, 0, 0, 0, 6, 0],
+  [8, 0, 0, 0, 6, 0, 0, 0, 3],
+  [4, 0, 0, 8, 0, 3, 0, 0, 1],
+  [7, 0, 0, 0, 2, 0, 0, 0, 6],
+  [0, 6, 0, 0, 0, 0, 2, 8, 0],
+  [0, 0, 0, 4, 1, 9, 0, 0, 5],
+  [0, 0, 0, 0, 8, 0, 0, 7, 9]];
+function getPossibleNumbers(puzzle, cell, missingGridNumbers){
+  let possible = [];
+  missingGridNumbers.filter((n)=>{
+    if(puzzle[cell.row].indexOf(n) === -1)
+      possible.push(n)
+    // for(var col = 0; col < 9; col++){
+    //   if(col > 0)
+    //     puzzle[cell.row][col]
+    // }
+
+
+  })
+  return possible
+
+}
+
 function sudoku(puzzle) {
 
 
@@ -7,19 +34,60 @@ function sudoku(puzzle) {
   //scan vertical and trim the list of previous possible numbers for this position.
   //if no clear one can be placed move to the next 0.
   //repeat but this time check where only one number is possible
-  let possibleMatrix = []
+  let possibleMatrix = [];
+  let filledGridValues = []
   for (let row = 0; row < 9; row++) {
+    let isABoundryRow = (row + 1) % 3 === 0;
+    let isFirstRow = (row) % 3 === 0;
+    let isMiddleRow = !isABoundryRow && !isFirstRow;
+
     for (let col = 0; col < 9; col++) {
+      let isABoundryCol = (col + 1) % 3 === 0;
+      let isFirstCol = (col) % 3 === 0;
+      let isMiddleCol = !isABoundryCol && !isFirstCol;
 
-      if( puzzle[row][col] === 0){
+      if (puzzle[row][col] === 0) {
         //get grid values
-        let isABoundryCol = (col+1) % 3  === 0;
-        let isFirstCol = (col) % 3  === 0;
-        let isMiddleCol = !isABoundryCol && !isFirstCol;
 
+        if (isABoundryCol && isFirstRow) {
+          let filledGridValues = [
+            puzzle[row][col - 2], puzzle[row][col - 1],
+            puzzle[row + 1][col - 2], puzzle[row + 1][col - 1], puzzle[row + 1][col],
+            puzzle[row + 2][col - 2], puzzle[row + 2][col - 1], puzzle[row + 2][col],
+          ].filter((e) => e > 0);
+        }
+        let missingGridNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9].filter((n) => filledGridValues.indexOf(n) === -1);
+        // console.log(getPossibleNumbers(puzzle, {row, col}, missingGridNumbers), "row:",row, "col:",col);
       }
+
     }
   }
+
+  function getGridValues(puzzle){
+    let grid = [];
+    for(var i = 0; i < 9; i++){
+    for(var j = 0; j < 9; j++){
+      if((j) % 3 === 0)
+        try{
+      grid[j] = [
+        [puzzle[i][j], puzzle[i][j+1], puzzle[i][j+2]],
+        [puzzle[i+1][j], puzzle[i+1][j+1], puzzle[i+1][j+2]],
+        [puzzle[i+2][j], puzzle[i+2][j+1], puzzle[i+2][j+2]]
+      ]
+        }catch(e){
+
+        }
+    }
+    }
+
+    // grid[0] = [
+    //   puzzle[0][0], puzzle[0][1], puzzle[0][2],
+    //   puzzle[1][0], puzzle[1][1], puzzle[1][2],
+    //   puzzle[2][0], puzzle[1][1], puzzle[2][2]
+    // ]
+    return grid;
+  }
+  console.log(getGridValues(puzzle));
 
   var puzzle2 = [
     [5, 3, 0, 0, 7, 0, 0, 0, 0],
